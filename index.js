8747b1d4231446041886d35c47b5cbce2b841828
para1ba <custodio_vitor@outlook.com>
2021-08-23 20:01:53 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+var cowsay = require("cowsay");
+
+function mimosa(message = "Instituto Biosistêmico") {
+    console.log(cowsay.say({
+        text : message,
+        e : "oO",
+        T : "U "
+    }))
+}
+
+module.exports = mimosa
+
+mimosa()
